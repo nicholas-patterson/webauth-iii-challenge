da6f180184d80626d607e743ecbc7f461ce766e0
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-18 15:57:52 -0400

    :sparkles: user router complete
    user router complete

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const authRouter = require("../auth/authRouter");
+const userRouter = require("../user/userRouter");
 const server = express();
 
 server.use(express.json());
+server.use("/api/auth", authRouter);
+server.use("/api/user", userRouter);
 
 module.exports = server;
---
+++ user/userRouter.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const User = require("./user-helpers");
+
+router.get("/", (req, res) => {
+  User.find().then(users => {
+    res.status(200).json(users);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  User.findById(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Server could not get user" });
+    });
+});
+
+module.exports = router;
